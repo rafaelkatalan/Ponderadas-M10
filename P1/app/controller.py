e98cb5abbf1653b8abc3e9ce7421c50f5f51d4d0
rafaelkatalan <99191700+rafaelkatalan@users.noreply.github.com>
2024-04-28 14:35:22 -0300

    .

---
+++ P1/app/controller.py
@@ -0,0 +1,16 @@
+from fastapi import FastAPI
+from service import *
+import uvicorn
+
+app = FastAPI()
+
+@app.get("/get")
+def read_root():
+    return get_data()
+
+@app.post("/post")
+def read_root(name:str, phone:str):
+    return post_data(name, phone)
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="localhost", port=80)
---
+++ P1/init.sql
@@ -0,0 +1,4 @@
+CREATE TABLE test (
+    name VARCHAR(100),
+    phone INT
+);
